c0875ba7ee8afc3359021bef37862437025c0b0c
navin <rnavinkumar5499@gmail.com>
2020-10-06 10:19:12 +0530

    first commit

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '630750025248-mnq9f2at8mjsqkl9l54dbuovgpfgg5t9.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'VCmxoeo-IGh3PYOMb8lGs1pd');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'https://google.com');
+
+?>
